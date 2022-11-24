4ba168beeff2536e7bec867c35e20c40535568f2
kira <kira@192.168.91.207>
2022-11-24 09:51:30 +0100

    sd

---
+++ LS01_Fahrkartenautomat/src/Kreis.java
@@ -0,0 +1,17 @@
+
+public class Kreis {
+
+	public static void main(String[] args) {
+		String str1 = "*";
+		String str2 = "**";
+		
+		System.out.printf("%4s\n", str2);
+		System.out.printf("%s", str1);
+		System.out.printf("%5s\n", str1);
+		System.out.printf("%s", str1);
+		System.out.printf("%5s\n", str1);
+		System.out.printf("%4s\n", str2);
+
+	}
+
+}
